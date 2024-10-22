96acab21259edc0c79afc0460170cf2441bc6fa5
Versha <vershadugg68@gmail.com>
2024-10-22 13:37:09 +0530

    no changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ function App() {
       <Routes>
       <Route path='/' element={<Hero/>} />
       <Route path='/hero' element={<Hero/>} />
-      <Route path='about' element={<About/>} />
-      <Route path='contact' element={<Contact />}/>
-      <Route path='services' element={<Services/>} />
-      <Route path='*' element={<NotFound/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact />}/>
+      <Route path='/services' element={<Services/>} />
+      <Route path='/*' element={<NotFound/>} />
       </Routes>
       <Footer/>
       </BrowserRouter>
